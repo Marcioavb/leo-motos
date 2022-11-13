03833f0f985dc639341d28a8759109f28a4cabe2
Marcio Moura <marcioavb.ms@gmail.com>
2022-11-13 19:17:17 -0300

    inicio projeto

---
+++ src/main/java/br/com/leomotos/leomotos/LeoMotosApplication.java
@@ -0,0 +1,13 @@
+package br.com.leomotos.leomotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeoMotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeoMotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leomotos/leomotos/LeoMotosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leomotos.leomotos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeoMotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
